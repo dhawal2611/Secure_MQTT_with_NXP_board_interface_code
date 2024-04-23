1ae11adbd52a29fcb2e5fb414677173349f706f8
Lad Dhawal Umesh <dhawal261195@gmail.com>
2024-04-23 06:20:42 +0000

    Update files

--- mqtt_publish/mqtt_publish.c
+++ mqtt_publish/mqtt_publish.c
@@ -1,25 +1,84 @@
 #include <stdio.h>
-#include "mosquitto.h"
+#include <mosquitto.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(){
-	int rc;
-	struct mosquitto * mosq;
+struct mosquitto *mosq = NULL;
+char *topic = NULL;
+int rv;
+int MQTT_PORT = 8883;
+int keepalive = 60;
+bool clean_session = true;
+char *CA_CERT = "/workspaces/test/certs/ca.crt";
+char *CLIENT_CRT = "/workspaces/test/certs/client.crt";
+char *CLIENT_KEY = "/workspaces/test/certs/client.key";
+char *MQTT_BROKER = "192.168.60.109";
+char *MQTT_TOPIC = "/topic";
 
+
+void mosq_log_callback(struct mosquitto *mosq, void *userdata, int level, const char *str)
+{
+	switch (level)
+	{
+	//case MOSQ_LOG_DEBUG:
+	//case MOSQ_LOG_INFO:
+	//case MOSQ_LOG_NOTICE:
+	case MOSQ_LOG_WARNING:
+	case MOSQ_LOG_ERR:
+	{
+		printf("%i:%s\n", level, str);
+	}
+	}
+}
+
+int mqtt_setup(void)
+{
 	mosquitto_lib_init();
-	mosq = mosquitto_new("publisher-test", true, NULL);
 
-	rc = mosquitto_connect(mosq, "localhost", 1883, 60);
-	if(rc != 0){
-		printf("Client could not connect to broker! Error Code: %d\n", rc);
-		mosquitto_destroy(mosq);
+	mosq = mosquitto_new(NULL, clean_session, NULL);
+	if (!mosq)
+	{
+		fprintf(stderr, "Error: Out of memory.\n");
 		return -1;
 	}
-	printf("We are now connected to the broker!\n");
 
-	mosquitto_publish(mosq, NULL, "test/t1", 6, "Hello", 0, false);
+	mosquitto_log_callback_set(mosq, mosq_log_callback);
 
-	mosquitto_disconnect(mosq);
-	mosquitto_destroy(mosq);
+	rv = mosquitto_tls_set(mosq, CA_CERT, NULL, CLIENT_CRT, CLIENT_KEY, NULL);
+	rv = mosquitto_tls_opts_set(mosq, 1, NULL, NULL);
+	rv = mosquitto_connect(mosq, MQTT_BROKER, MQTT_PORT, keepalive);
+
+	if (rv != 0)
+		fprintf(stderr, "mosquitto_connect failed. rv = %d\n", rv);
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	if (mqtt_setup() == 0)
+	{
+		printf("Setup done, connected to broker. \n");
+
+		rv = mosquitto_publish(mosq, NULL, MQTT_TOPIC, 11, "hello world", 0, false);
+
+		if (rv != 0)
+		{
+			fprintf(stderr, "mosquitto_publish failed. rv = %d\n", rv);
+		}
+		else
+		{
+			printf("mosquitto_publish successful. \n");
+		}
+		mosquitto_loop_forever(mosq, -1, 1);
+		return 0;
+	}
+	else
+	{
+		printf("setup failed");
+		return -1;
+	}
 
 	mosquitto_lib_cleanup();
 	return 0;
