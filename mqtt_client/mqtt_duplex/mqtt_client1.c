f2483adb3b7907db18045078acc622d6fd62d28d
Rohit Sangale <rohit.sangale_1@nxp.com>
2024-04-26 14:21:39 +0000

    Added Support for cJSON Library and working example

--- mqtt_client/mqtt_duplex/mqtt_client1.c
+++ mqtt_client/mqtt_duplex/mqtt_client1.c
@@ -9,6 +9,7 @@
 #include <netinet/in.h>
 #include <net/if.h>
 #include <arpa/inet.h>
+#include <cjson/cJSON.h>
 
 struct mosquitto *mosq = NULL;
 char *topic = NULL;
@@ -19,9 +20,9 @@ bool clean_session = true;
 char *CA_CERT = "../../../mqtt_broker/certs/ca.crt";
 char *CLIENT_CRT = "../../../mqtt_broker/certs/client.crt";
 char *CLIENT_KEY = "../../../mqtt_broker/certs/client.key";
-char MQTT_BROKER[64] = "172.17.0.1"; // IP address of the system on which broker is running
-char *MQTT_TOPIC1 = "/topic1";
-char *MQTT_TOPIC2 = "/topic2";
+char MQTT_BROKER[64] = "192.168.1.16"; // IP address of the system on which broker is running
+char *MQTT_TOPIC1 = "req_config_data";
+char *MQTT_TOPIC2 = "config_data";
 
 #define MQTT_QOS 1
 #define MQTT_RETAIN 0
@@ -51,13 +52,28 @@ uint8_t u8GetIP() {
 }
 
 void message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message) {
-    if(message->payloadlen){
-        printf("%s %.*s\n", message->topic, (int)message->payloadlen, (char *)message->payload);
-        strncpy (data, (char *)message->payload, (int)message->payloadlen);
-        flag = 1;
-    } else {
-        printf("%s (null)\n", message->topic);
-    }
+
+    cJSON *config_data = cJSON_Parse((char*)message->payload);
+
+    cJSON *fw_version_data = cJSON_GetObjectItem(config_data, "fw_version");
+    cJSON *serial_no_data = cJSON_GetObjectItem(config_data, "serial_number");
+
+    printf("\r\nreceived FW Version: %02x\r\n", fw_version_data->valueint);
+    printf("\r\nreceived FW Version: %s\r\n", serial_no_data->valuestring);
+
+    cJSON_Delete(config_data);
+}
+
+void display_menu() 
+{
+    // Display the header
+    printf("MQTT Client Example\n");
+    printf("-------------------\n");
+
+    // Display the options
+    printf("Options:\n");
+    printf("1. Request Configuration data\n");
+    printf("2. Exit\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -65,7 +81,7 @@ int main(int argc, char *argv[]) {
     int rc;
 
     if(u8GetIP() == -1) {
-        printf("Invalide Interface name\n");
+        printf("Invalid Interface name\n");
         return 0;
     }
     printf("IP is successfully\n");
@@ -90,23 +106,53 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    mosquitto_subscribe(mosq, NULL, MQTT_TOPIC1, MQTT_QOS);
+    mosquitto_subscribe(mosq, NULL, MQTT_TOPIC2, MQTT_QOS);
 
     mosquitto_loop_start(mosq);
 
     // Publish a message
     //char payload[] = "Hello MQTT";
     //mosquitto_publish(mosq, NULL, MQTT_TOPIC2, sizeof(payload), payload, MQTT_QOS, MQTT_RETAIN);
-
+    display_menu();
+    printf("Enter your choice: ");
     // Wait for messages to arrive
-    while(1) {
-        if(flag && (strstr(data , "Hello MQTT1") != NULL)) {
-            char payload[] = "Hello MQTT2";
-            mosquitto_publish(mosq, NULL, MQTT_TOPIC2, sizeof(payload), payload, MQTT_QOS, MQTT_RETAIN);
+    while (1)
+    {
+        
+
+        // Get user input
+        int choice;
+        
+        scanf("%d", &choice);
+
+        // Process the user choice
+        switch (choice)
+        {
+        case 1:
+            printf("Requesting Configuration data...\n");
+            cJSON *req_config_data = cJSON_CreateObject();
+            if(req_config_data != NULL)
+            {
+                cJSON_AddBoolToObject(req_config_data,"req_fw_version", true);
+                cJSON_AddBoolToObject(req_config_data,"req_serial_no", true);
+            }
+            char* payload = cJSON_Print(req_config_data);
+            // Call function to request configuration data
+            //printf("sending payload: %s with size: %lu", payload, strlen(payload));
+            mosquitto_publish(mosq, NULL, MQTT_TOPIC1, strlen(payload), payload, MQTT_QOS, MQTT_RETAIN);
             flag = 0;
+            cJSON_Delete(req_config_data);
+            printf("\r\nresponse: \r\n");
+
+            break;
+        case 2:
+            printf("Exiting...\n");
+            // Call function to exit
+            break;
+        default:
+            printf("Invalid choice!\n");
+            break;
         }
-        // Main loop can be used for other tasks
-        usleep(1000000);
     }
 
     mosquitto_destroy(mosq);
