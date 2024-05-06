8c1b6c6a7f23c1deb12f89eb3feb72f0a790a4e1
Rohit Sangale <rohit.sangale_1@nxp.com>
2024-05-06 12:34:54 +0000

    Updated the colors for client

--- mqtt_client/mqtt_duplex/mqtt_client1.c
+++ mqtt_client/mqtt_duplex/mqtt_client1.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdarg.h>
+#include <errno.h>
 #include <ctype.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -13,10 +15,20 @@
 #include <net/if.h>
 #include <arpa/inet.h>
 #include <cjson/cJSON.h>
+#include <time.h>
 
 
 #define MQTT_DEBUG  0
 #define BUFFER_SIZE 64
+
+// ANSI color codes
+#define RED_COLOR "\033[1;31m"      /**< Red color Define */
+#define GREEN_COLOR "\033[1;32m"    /**< Green color Define */
+#define RESET_COLOR "\033[0m"       /**< Reset color Define */
+#define BLUE_COLOR "\033[1;34m"     /**< Blue color Define */
+
+#define ANSI_COLOR_RED     "\x1b[31m"   /**< Ansi Color Red */
+#define ANSI_COLOR_RESET   "\x1b[0m"    /**< Ansi color reset */
 
 struct mosquitto *mosq = NULL;
 char *topic = NULL;
@@ -31,12 +43,92 @@ char MQTT_BROKER[BUFFER_SIZE] = {0}; // IP address of the system on which broker
 char *MQTT_TOPIC1 = "req_config_data";
 char *MQTT_TOPIC2 = "config_data";
 bool tls_flag = false;
+bool timeout_flag = false;
 
 #define MQTT_QOS 1
 #define MQTT_RETAIN 0
 
 char data[1024];
 uint8_t flag = 0;
+
+/**
+ * @brief Debug Error
+ * 
+ * @param prefix 
+ */
+void _debugError(const char *prefix)
+{
+    fprintf(stderr," ==> %s%s: %s%s\n", RED_COLOR, prefix, strerror(errno), RESET_COLOR);
+
+}
+
+/**
+ * @brief Function to print blue text to a file pointer
+ * 
+ * @param file 
+ * @param format 
+ * @param ... 
+ */
+void _fprintfBlue(FILE *file, const char *format, ...) 
+{
+    va_list args;
+    va_start(args, format);
+    fprintf(file,BLUE_COLOR);
+    vfprintf(file,format,args);
+    fprintf(file,RESET_COLOR);
+    fflush(file);
+    va_end(args);
+}
+
+/**
+ * @brief Function to print Red text to a file pointer
+ * 
+ * @param file 
+ * @param format 
+ * @param ... 
+ */
+void _fprintfRed(FILE *file, const char *format, ...) 
+{
+    va_list args;
+    va_start(args, format);
+    fprintf(file,RED_COLOR);
+    vfprintf(file,format,args);
+    fprintf(file,RESET_COLOR);
+    fflush(file);
+    va_end(args);
+}
+
+/**
+ * @brief Function to print Green text to a file pointer
+ * 
+ * @param file 
+ * @param format 
+ * @param ... 
+ */
+void _fprintfGreen(FILE *file, const char *format, ...) 
+{
+    va_list args;
+    va_start(args, format);
+    fprintf(file,GREEN_COLOR);
+    vfprintf(file,format,args);
+    fprintf(file,RESET_COLOR);
+    fflush(file);
+    va_end(args);
+}
+
+
+
+/**
+ * @brief Print Header for Chat
+ * @callergraph
+ * 
+ */
+void print_header()
+{
+    _fprintfBlue(stdout,"******************************* MQTT Client Example ***************************************\n");
+    _fprintfBlue(stdout,"*DEMO Application to get Config Data from RMM device                                      *\n");
+    _fprintfBlue(stdout,"*******************************************************************************************\n");
+}
 
 bool is_zero_initialized(const char *buffer, size_t size) 
 {
@@ -68,12 +160,12 @@ uint8_t u8GetIP() {
     strncpy(ifr.ifr_name , interface_name , IFNAMSIZ - 1);
     ioctl(n, SIOCGIFADDR, &ifr);
     close(n);
-    printf("IP Address of Interface %s - %s\n" , interface_name , inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr )->sin_addr));
+    _fprintfGreen(stdout,"IP Address of Interface %s - %s\n" , interface_name , inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr )->sin_addr));
     if(strstr(inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr )->sin_addr), "0.0.0.0") != NULL) {
         return -1;
     }
     sprintf(MQTT_BROKER, "%s", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr )->sin_addr));
-    printf("IP Address of broker is %s\n" , MQTT_BROKER);
+    _fprintfGreen(stdout,"IP Address of broker is %s\n" , MQTT_BROKER);
     return 0;
 }
 
@@ -81,14 +173,15 @@ void message_callback(struct mosquitto *mosq, void *userdata, const struct mosqu
 
 #if MQTT_DEBUG
     if(message->payloadlen){
-        printf("%s %.*s\n", message->topic, (int)message->payloadlen, (char *)message->payload);
+        _fprintfGreen(stdout,"%s %.*s\n", message->topic, (int)message->payloadlen, (char *)message->payload);
         strncpy (data, (char *)message->payload, (int)message->payloadlen);
         flag = 1;
     } else {
         printf("%s (null)\n", message->topic);
     }
 #endif
-    printf("\r\nresponse: \r\n");
+    timeout_flag = true;
+    _fprintfBlue(stdout,"\r\nresponse: \r\n");
     cJSON *config_data = cJSON_Parse((char*)message->payload);
 
     if(config_data != NULL)
@@ -96,12 +189,12 @@ void message_callback(struct mosquitto *mosq, void *userdata, const struct mosqu
         cJSON *fw_version_data = cJSON_GetObjectItem(config_data, "fw_version");
         if (fw_version_data != NULL)
         {
-            printf("\r\nreceived FW Version: %02x\r\n", fw_version_data->valueint);
+            _fprintfGreen(stdout,"\r\nreceived FW Version: %02x\r\n", fw_version_data->valueint);
         }
         cJSON *serial_no_data = cJSON_GetObjectItem(config_data, "serial_number");
         if (serial_no_data != NULL)
         {
-            printf("\r\nreceived Serial No: %s\r\n", serial_no_data->valuestring);
+            _fprintfGreen(stdout,"\r\nreceived Serial No: %s\r\n", serial_no_data->valuestring);
         }
         cJSON *usb_mode_data = cJSON_GetObjectItem(config_data, "usb_mode");
         if (usb_mode_data != NULL)
@@ -109,23 +202,23 @@ void message_callback(struct mosquitto *mosq, void *userdata, const struct mosqu
             switch (usb_mode_data->valueint)
             {
             case 0:
-                printf("\r\nreceived USB Mode: USB_DeviceStateDisable\r\n");
+                _fprintfGreen(stdout,"\r\nreceived USB Mode: USB_DeviceStateDisable\r\n");
                 break;
 
             case 1:
-                printf("\r\nreceived USB Mode: USB_DeviceStateDownstream\r\n");
+                _fprintfGreen(stdout,"\r\nreceived USB Mode: USB_DeviceStateDownstream\r\n");
                 break;
 
             case 2:
-                printf("\r\nreceived USB Mode: USB_DeviceStateDiagnostics\r\n");
+                _fprintfGreen(stdout,"\r\nreceived USB Mode: USB_DeviceStateDiagnostics\r\n");
                 break;
 
             case 3:
-                printf("\r\nreceived USB Mode: USB_HostStateUpstream\r\n");
+                _fprintfGreen(stdout,"\r\nreceived USB Mode: USB_HostStateUpstream\r\n");
                 break;
 
             default:
-                printf("\r\nreceived USB Mode: UNKNOWN\r\n");
+                _fprintfRed(stdout,"\r\nreceived USB Mode: UNKNOWN\r\n");
                 break;
             }
         }
@@ -142,13 +235,12 @@ void message_callback(struct mosquitto *mosq, void *userdata, const struct mosqu
 void display_menu() 
 {
     // Display the header
-    printf("MQTT Client Example\n");
-    printf("-------------------\n");
 
     // Display the options
-    printf("Options:\n");
-    printf("1. Request Configuration data\n");
-    printf("2. Exit\n");
+    _fprintfBlue(stdout,"Options:\n");
+    _fprintfBlue(stdout,"1. Request Configuration data\n");
+    _fprintfBlue(stdout,"2. Request Incorrect Configuration Data \n");
+    _fprintfBlue(stdout,"3. Exit\n");
 }
 
 
@@ -156,13 +248,13 @@ int mqtt_connect()
 {
     struct mosquitto *mosq;
     int rc;
-    printf("IP is successfully\n");
+    _fprintfGreen(stdout,"IP is obtained successfully\n");
 
     mosquitto_lib_init();
 
     mosq = mosquitto_new(NULL, true, NULL);
     if(!mosq) {
-        fprintf(stderr, "Error: Out of memory.\n");
+        _fprintfRed(stderr, "Error: Out of memory.\n");
         return 1;
     }
 
@@ -173,11 +265,11 @@ int mqtt_connect()
 
     mosquitto_message_callback_set(mosq, message_callback);
 
-    printf("MQTT Broker is : %s\n", MQTT_BROKER);
+    _fprintfGreen(stdout,"MQTT Broker is : %s\n", MQTT_BROKER);
 
     rc = mosquitto_connect(mosq, MQTT_BROKER, MQTT_PORT, 60);
     if(rc != MOSQ_ERR_SUCCESS) {
-        fprintf(stderr, "Error: Could not connect to MQTT broker. Return code: %d\n", rc);
+        _fprintfRed(stderr, "Error: Could not connect to MQTT broker. Return code: %d\n", rc);
         return 1;
     }
 
@@ -190,13 +282,13 @@ int mqtt_connect()
 int get_port()
 {
     char input[2];
-    printf("Select MQTT Port: (1 or 2)\n");
+    _fprintfBlue(stdout,"Select MQTT Port: (1 or 2)\n");
     while(1)
     {
-        printf("1. Secure MQTT Port (8883)\n");
-        printf("2. Unsecure MQTT Port (1883)\n");
+        _fprintfBlue(stdout,"1. Secure MQTT Port (8883)\n");
+        _fprintfBlue(stdout,"2. Unsecure MQTT Port (1883)\n");
         if (scanf("%1s", input) != 1) { // Read a single character
-            printf("Invalid input. Please try again.\n");
+            _fprintfRed(stderr,"Invalid input. Please try again.\n");
             while (getchar() != '\n'); // Clear the input buffer
             continue;
         }
@@ -205,7 +297,7 @@ int get_port()
         if (strcmp(input, "1") == 0 || strcmp(input, "2") == 0) {
             break; // Valid input, exit the loop
         } else {
-            printf("Invalid input. Please try again.\n");
+            _fprintfRed(stderr,"Invalid input. Please try again.\n");
         }
     }
 
@@ -232,23 +324,56 @@ int get_port()
 
 }
 
+void timeout_func()
+{
+    time_t start_time, current_time;
+    double elapsed_time;
+
+    // Get the current time
+    time(&start_time);
+
+    // Start the timer for 10 seconds
+    while (true) {
+        // Check if the flag is turned on
+        if (timeout_flag == true) {
+            timeout_flag = false;
+            break;
+        }
+
+        // Get the current time
+        time(&current_time);
+
+        // Calculate elapsed time
+        elapsed_time = difftime(current_time, start_time);
+
+        // Check if 10 seconds have elapsed
+        if (elapsed_time >= 10.0) {
+            _fprintfRed(stderr,"Timeout: MQTT data not received\n");
+            break;
+        }
+    }
+
+}
+
 int main(int argc, char *argv[]) {
     
     char input[2];
 
+    print_header();
+
     //Get the MQTT Broker Address and Port
-    printf("Auto Getting the MQTT Broker Address... \n");
+    _fprintfBlue(stdout,"Auto Getting the MQTT Broker Address... \n");
 
     if(u8GetIP() == -1) 
     {
-        printf("Invalid Interface name\n");
+        _fprintfRed(stdout,"Invalid Interface name\n");
         //return 0;
     }
     while(1)
     {
-        printf("Is this MQTT Broker Address Correct? : %s (y/n)\t", MQTT_BROKER);
+        _fprintfBlue(stdout,"Is this MQTT Broker Address Correct? : %s (y/n)\t", MQTT_BROKER);
         if (scanf("%1s", input) != 1) { // Read a single character
-            printf("Invalid input. Please try again.\n");
+            _fprintfRed(stdout,"Invalid input. Please try again.\n");
             while (getchar() != '\n'); // Clear the input buffer
             continue;
         }
@@ -258,7 +383,7 @@ int main(int argc, char *argv[]) {
         if (strcmp(input, "y") == 0 || strcmp(input, "n") == 0) {
             break; // Valid input, exit the loop
         } else {
-            printf("Invalid input. Please try again.\n");
+            _fprintfRed(stdout,"Invalid input. Please try again.\n");
         }
     }
 
@@ -277,12 +402,12 @@ int main(int argc, char *argv[]) {
         {
             // Ask the user to manually enter the addr
             memset(MQTT_BROKER, 0x00, BUFFER_SIZE);
-            printf("Enter Server Address: ");
+            _fprintfBlue(stdout,"Enter Server Address: ");
             scanf("%63s", MQTT_BROKER);
-            printf("Is this MQTT Broker Address Correct? : %s (y/n)\t", MQTT_BROKER);
+            _fprintfBlue(stdout,"Is this MQTT Broker Address Correct? : %s (y/n)\t", MQTT_BROKER);
             if (scanf("%1s", input) != 1)
             { // Read a single character
-                printf("Invalid input. Please try again.\n");
+                _fprintfRed(stdout,"Invalid input. Please try again.\n");
                 while (getchar() != '\n')
                     ; // Clear the input buffer
                 continue;
@@ -296,7 +421,7 @@ int main(int argc, char *argv[]) {
             }
             else
             {
-                printf("Invalid input. Please try again.\n");
+                _fprintfRed(stdout,"Invalid input. Please try again.\n");
             }
         }
 
@@ -307,7 +432,7 @@ int main(int argc, char *argv[]) {
     }
     
     display_menu();
-    printf("Enter your choice: ");
+    _fprintfBlue(stdout,"Enter your choice: ");
     // Wait for messages to arrive
     while (1)
     {
@@ -322,7 +447,7 @@ int main(int argc, char *argv[]) {
         switch (choice)
         {
         case 1:
-            printf("Requesting Configuration data...\n");
+            _fprintfBlue(stdout,"Requesting Configuration data...\n");
             cJSON *req_config_data = cJSON_CreateObject();
             if(req_config_data != NULL)
             {
@@ -336,15 +461,34 @@ int main(int argc, char *argv[]) {
             mosquitto_publish(mosq, NULL, MQTT_TOPIC1, strlen(payload), payload, MQTT_QOS, MQTT_RETAIN);
             flag = 0;
             cJSON_Delete(req_config_data);
+            //Implement a timeout function
+            timeout_func();
 
             break;
         case 2:
-            printf("Exiting...\n");
+        cJSON *req_w_config_data = cJSON_CreateObject();
+            if(req_w_config_data != NULL)
+            {
+                cJSON_AddBoolToObject(req_config_data,"random_topic", true);
+                cJSON_AddBoolToObject(req_config_data,"lorem_ipsum", true);
+                cJSON_AddBoolToObject(req_config_data,"acv", true);
+            }
+            char* w_payload = cJSON_Print(req_w_config_data);
+            // Call function to request configuration data
+            //printf("sending payload: %s with size: %lu", payload, strlen(payload));
+            mosquitto_publish(mosq, NULL, MQTT_TOPIC1, strlen(w_payload), w_payload, MQTT_QOS, MQTT_RETAIN);
+            flag = 0;
+            cJSON_Delete(req_w_config_data);
+            timeout_func();
+
+            break;
+        case 3:
+            _fprintfRed(stdout,"Exiting...\n");
             // Call function to exit
             exit(0);
             break;
         default:
-            printf("Invalid choice!\n");
+            _fprintfRed(stdout,"Invalid choice!\n");
             break;
         }
     }
